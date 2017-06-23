f92c13b69ab59e9cc1556da437664dcbcce2747b
ceeswolfs <ceeswolfs1985@gmail.com>
2017-06-23 22:52:17 +0200

    Fix documentatie

--- LightTrain.ino
+++ LightTrain.ino
@@ -15,7 +15,7 @@
 #define UIT pixels.Color(0,0,0)
 
 #if (delayval >= 200)
-#define KLEUR pixels.Color(0, 150, 0)
+#define KLEUR pixels.Color(0, 150, 0) // Groen de trein 
 #elif (delayval < 200) 
 #define KLEUR pixels.Color(200,144,0)
 #elif (delayval < 150) 
@@ -48,31 +48,30 @@ void setup() {
 
 void loop() {
   sensor1 = analogRead(A0);
-  while (sensor1 < SENSOR_THRESHOLD) {
+  while (sensor1 < SENSOR_THRESHOLD) { // Sensor is laag, de trein komt langs
     ms++;
     delay(1);
     trein_langs = true;
-    sensor1 = analogRead(A0);
+    sensor1 = analogRead(A0); // Herhaal deze loop totdat de waarde waar hoog is, dus totdat de trein voorbij is
   }
-  aantal_leds = ms/delayval;
-  if(trein_langs) {
-    for(int i=0;i<NUMPIXELS+aantal_leds;i++){
-      int achterste_led = i - aantal_leds;
-      if(achterste_led >= 0) {
+  aantal_leds = ms/delayval;  // Het aantal ledjes dat de trein breedt is hoelang de trein overover doet gedeeld door hoelang een ledje duurt
+  if(trein_langs) {           // Als er een trein langsgekomen is doe het volgende:
+    for(int i=0;i<NUMPIXELS+aantal_leds;i++){ // Beetje ingewikkeld, tel tot het aantal ledjes + het aantal ledjes dat de trein lang is
+      int achterste_led = i - aantal_leds;    // Dit doe ik zo om ook het terugschakelen van de ledjes te regelen
+      if(achterste_led >= 0) {                // Als de trein helemaal op de ledstrip is moeten er weer lampjes op de rust kleur worden gezet
         pixels.setPixelColor(achterste_led, WIT);
       }
-      if (i < NUMPIXELS) {
+      if (i < NUMPIXELS) {                    // Zet de lampjes die met de trein meegaan op de kleur aangeven door de snelheid, zie bovenin
         pixels.setPixelColor(i, KLEUR);
       } 
       pixels.show();
-      delay(delayval); 
+      delay(delayval);                        // Wacht delayval milliseconden
     }
-    trein_langs = false;
+    trein_langs = false;                      // Trein is langs dus doe dit niet weer opnieuw
     ms = 0;
-    laatste_trein = millis();
+    laatste_trein = millis();                 // Reset als het ware de timer die het uitschakelen van de ledjes regelt, zie hieronder
   }
-  if((millis() - laatste_trein) > IDLE_TIME) {
-    // De trein is al lang niet langsgeweest, zet de lampjes uit
+  if((millis() - laatste_trein) > IDLE_TIME) {  // Als de trein IDLE_TIME milliseconden niet is langsgeweest, zet de lampjes uit
     for(int i=0;i<NUMPIXELS;i++){
       // Zet de lampjes uit
       pixels.setPixelColor(i, UIT);
