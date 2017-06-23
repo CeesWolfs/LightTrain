ff238e3309d20c47ad89e8269d822d51a890fd0f
ceeswolfs <ceeswolfs1985@gmail.com>
2017-06-23 10:45:25 +0200

    Update LightTrain.ino

--- LightTrain.ino
+++ LightTrain.ino
@@ -18,41 +18,38 @@
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 const int sensor1;
-const int sensor2;
-int delayval = 500; // delay for half a second
+int delayval = 200; // delay for half a second
 int ms = 0;         // Aantal milliseconden dat de trein over het traject sensor1 -> sensor2 doet
-float snelheid;
+int aantal_leds;
+boolean trein_langs;
 
 void setup() {
   pixels.begin(); // This initializes the NeoPixel library.
   for(int i=0;i<NUMPIXELS;i++){
     // Zet de beginkleur op wit
-    pixels.setPixelColor(i, pixels.Color(255, 255, 255)); // Moderately bright green color.
+    pixels.setPixelColor(i, pixels.Color(255, 255, 255));
   }
   pixels.show(); 
 }
 
 void loop() {
   sensor1 = analogRead(A0);
-  sensor2 = analogRead(A1);
   while (sensor1 > SENSOR_THRESHOLD) {
     ms++;
-    sensor2 = analogRead(A1);
-    if(sensor2 < SENSOR_THRESHOLD) break;
     delay(1);
+    trein_langs = true;
+    sensor1 = analogRead(A0);
   }
-  snelheid = AFSTAND_TRAJECT/(float)ms;
-  delayval = 1.7*snelheid; // Snelheid is in cm/ms astand tussen de ledjes is 1,7 seconden dus delay in ms is 1,7*snelheid
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  aantal_leds = ms/delayval;
+  if(trein_langs) {
+    for(int i=0;i<NUMPIXELS;i++){
 
-  for(int i=0;i<NUMPIXELS;i++){
+      // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+      pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
 
-    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+      pixels.show(); // This sends the updated pixel color to the hardware.
 
-    pixels.show(); // This sends the updated pixel color to the hardware.
-
-    delay(delayval); // Delay for a period of time (in milliseconds).
+      delay(delayval); // Delay for a period of time (in milliseconds).
 
   }
 }
