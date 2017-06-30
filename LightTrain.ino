4b1f0d69d86b9daf22f881b264e5a41d3d45ea5c
ceeswolfs <ceeswolfs1985@gmail.com>
2017-06-30 20:02:23 +0200

    Update LightTrain.ino

--- LightTrain.ino
+++ LightTrain.ino
@@ -11,15 +11,17 @@
 #define NUMPIXELS      10      // Aantal ledjes in de neopixel ledstring
 #define SENSOR_THRESHOLD 400   // Waarde waaronder de sensor komt als er een trein langsgaat, expirimenteel bepaald
 
+#define DELAY 200              // Wacht 200 milliseconden tussen elk lampje, verander dit afhankelijk van de snelheid van de trein
+
 #define IDLE_TIME 300L*1000L   // Na 5 minuten moeten de lampjes weer uit gaan
 #define WIT kleur(150,150,150) 
 #define UIT kleur(0,0,0)
 
-#if (delayval >= 200)
+#if (DELAY >= 200)
 #define KLEUR kleur(0, 200, 0) // Groen de trein gaat langzaam
-#elif (delayval >= 150)
+#elif (DELAY >= 150)
 #define KLEUR kleur(200, 200, 0) // Geel de trein gaat niet snel
-#elif (delayval >= 100)
+#elif (DELAY >= 100)
 #define KLEUR kleur(200, 120, 0) // Oranje de trein gaat vrij snel 
 #else
 #define KLEUR kleur(200, 0, 0)   // Rood de trein gaat vol gas
@@ -30,7 +32,6 @@
 // example for more information on possible values.
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-const int delayval = 200; // delay for half a second
 unsigned long laatste_trein;   // Aantal milliseconden sinds de laatste keer dat de trein langskwam.
 
 
@@ -58,7 +59,7 @@ void loop() {
     sensor1 = analogRead(A0); // Herhaal deze loop totdat de waarde waar hoog is, dus totdat de trein voorbij is
     trein_komt = true;  // Er komt een trein aan
   }
-  aantal_leds = ms / delayval; // Het aantal ledjes dat de trein breedt is = hoelang de trein overover doet gedeeld door hoelang een ledje duurt
+  aantal_leds = ms / DELAY; // Het aantal ledjes dat de trein breedt is = hoelang de trein overover doet gedeeld door hoelang een ledje duurt
   if (trein_komt) {          // Als er een trein aankomt is doe het volgende:
     while (sensor2 > SENSOR_THRESHOLD) { // Wacht totdat de trein bij het begin van de ledstrip is
        sensor2 = analogRead(A1);
@@ -72,7 +73,7 @@ void loop() {
         pixels.setPixelColor(i, KLEUR);
       }
       pixels.show();
-      delay(delayval);                        // Wacht delayval milliseconden
+      delay(DELAY);                           // Wacht delayval milliseconden
     }
     laatste_trein = millis();                 // Reset als het ware de timer die het uitschakelen van de ledjes regelt, zie hieronder
   }
